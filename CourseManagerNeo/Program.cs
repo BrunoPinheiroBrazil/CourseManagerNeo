b81ee59237384d5c6f311646b975a7e7bb50881c
BrunoPinheiroBrazil <brunopinheirobrazil@gmail.com>
2024-07-17 18:32:54 -0300

    updated minimalist program class

--- CourseManagerNeo/Program.cs
+++ CourseManagerNeo/Program.cs
@@ -1,17 +1,46 @@
-namespace CourseManager
-{
-  public class Program
-  {
-    public static void Main(string[] args)
-    {
-      CreateHostBuilder(args).Build().Run();
-    }
+using CourseManager;
+using CourseManager.DataBase.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-      Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder =>
-        {
-          webBuilder.UseStartup<Startup>();
-        });
-  }
+var builder = WebApplication.CreateBuilder(args);
+
+ConfigurationManager config = builder.Configuration;
+
+builder.Services.AddControllersWithViews().AddNewtonsoftJson();
+builder.Services.AddMvc().AddApplicationPart(typeof(Assembly).Assembly);
+builder.Services.AddDependencyServices();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CourseManagerDbContext>(options =>
+{
+  options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=BrunoEstudos;Trusted_Connection=True;MultipleActiveResultSets=true");
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+  app.UseDeveloperExceptionPage();
+else
+  app.UseExceptionHandler("/Error");
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllerRoute(
+  name: "default",
+  pattern: "{controller}/{action=Index}/{id?}"
+);
+
+using (var svcScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+{
+  var context = svcScope.ServiceProvider.GetService<CourseManagerDbContext>();
+  if (context == null)
+    throw new Exception("Could not create DBContext");
+
+  context.Database.Migrate();
 }
+
+app.Run();
+
+public partial class Program { }
